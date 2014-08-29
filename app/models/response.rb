f7c2fb16ce6721e05038dbd4704b0bc6bcfba59f
Edwardo Kimmy & garrett dalton <pair+eddiekimosabe.garrettdalton@devbootcamp.com>
2014-08-29 12:22:04 -0400

    Setup association between user and responses.
    -Added column user_id to responses table

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,6 @@
 class Response < ActiveRecord::Base
   belongs_to :prompt
+  belongs_to :user
 
   validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829013523) do
+ActiveRecord::Schema.define(version: 20140829162043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140829013523) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "responses", force: true do |t|
@@ -28,6 +29,16 @@ ActiveRecord::Schema.define(version: 20140829013523) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "prompt_id"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
