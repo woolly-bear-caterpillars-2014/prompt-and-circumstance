9488e1560f9ac627a307a5c8386dfc589ceb680f
Alexei Pesic <aly.pesic@gmail.com>
2014-08-29 00:30:39 -0400

    Created seed file for prompts and responses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  prompt = Prompt.create(
+    :title => Faker::Company.catch_phrase,
+    :description => Faker::Lorem.paragraph
+  )
+  rand(4..10).times do
+    prompt.responses.create(body: Faker::Hacker.say_something_smart)
+  end
+end
