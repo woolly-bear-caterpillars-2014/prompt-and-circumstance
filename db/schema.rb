3c4b51a3c207043ff3ac1259814fd56d2a1a5370
Edwardo Kimmy & garrett dalton <pair+eddiekimosabe.garrettdalton@devbootcamp.com>
2014-08-29 21:05:17 -0400

    Created Vote model, table, associations to Prompt, Response, and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829162043) do
+ActiveRecord::Schema.define(version: 20140830005325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20140829162043) do
     t.string   "email"
     t.string   "password_digest"
     t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "polarity"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
