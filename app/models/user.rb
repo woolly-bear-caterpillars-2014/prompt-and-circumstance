cf1eb5229b8b0686d63c6ff4c7c3fc785138eb28
Alexei Pesic <aly.pesic@gmail.com>
2014-08-30 02:36:46 -0400

    Refactored user controller tests. Removed redundant login method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,6 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false  }
   validates :password, length: { minimum: 6 }
+  validates :name, presence: true
 
-  # def self.authenticate(email, password)
-  #   user = User.find_by_email(email)
-  #   return user if user && (user.authenticate(password))
-  #   nil
-  # end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,43 +1,69 @@
 require 'rails_helper'
 
 describe UsersController do
-  context "create" do
+
+  context "get user signup page" do
     before do
-      post :create, :user => {
-        :name => "Bob",
-        :email => "bob@bob.com",
-        :password => "1234567"
-      }
+      get :new
     end
 
-    it "creates a new valid user" do
-      expect(User.last.name).to eq("Bob")
-    end
+    it { should render_template(:new) }
 
-    it { should respond_with(:redirect) }
-    it { should redirect_to(root_url) }
+    it "assigns new user to variable" do
+      expect(assigns(:user)).to be
+    end
   end
-  context "new user session" do
+
+  context "create with valid params" do
+    params = {:user => {
+      :name => "Bob",
+      :email => "bob@bob.com",
+      :password => "1234567"
+    }}
+
     before do
-      post :create, :user => {
-        :name => "Bob",
-        :email => "bob@bob.com",
-        :password => "1234567"
-      }
+      session.clear
+      post :create, params
     end
+
+    it "creates a new user" do
+      expect{
+	post :create,
+	:user => {
+	  :name => "Bob2",
+	  :email => "bob2@bob.com",
+	  :password => "1234567"
+	}
+      }.to change{ User.all.count }.by(1)
+    end
+
+    it { should redirect_to(root_path) }
+
     it "creates a new session when a user is created" do
       expect(session[:user_id]).to be
     end
   end
-  # context "user login session" do
-  #   before do
-  #     post :login, :user => {
-  #       :email => "bob@bob.com",
-  #       :password => "1234567"
-  #     }
-  #   end
-  #   it "creates a new session when a user logs in" do
-  #     expect(session[:user_id]).to be
-  #   end
-  #end
+
+  context "create with invalid params" do
+    params = {:user => {
+	:name => "",
+	:email => "bob@bob.com",
+	:password => "1234567"
+      }}
+
+    before do
+      session.clear
+      post :create, params
+    end
+
+    it "does not creates a new user" do
+      expect{ post :create, params }.not_to change{ User.all.count }
+    end
+
+    it { should render_template(:new) }
+
+    it "does not create a new session" do
+      expect(session[:user_id]).to be_nil
+    end
+  end
 end
