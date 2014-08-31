5cbdff6fc111333555ecbe408b12e0b71e92d65c
Alexei Pesic <aly.pesic@gmail.com>
2014-08-31 17:17:39 -0400

    Added error messages to login and signup pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to "/"
     else
-      render :new, :alert => "Invalid email or password"
+      redirect_to login_path, :alert => "Invalid email or password"
     end
   end
 
