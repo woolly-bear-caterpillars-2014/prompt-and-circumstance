0fe24e5865a46a18fa4085ec70c5d92d180cf7a7
eddiekimosabe <eddiekimosabe@gmail.com>
2014-09-01 03:09:13 -0400

    Added tests for response model

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe Response do
+	let(:user){ FactoryGirl.create(:user) }
+	let(:prompt){user.prompts.create(:title => "Beagles", :description => "How awesome are beagles?" )}
+	let(:response){ prompt.responses.create(:body => "Really awesome!", :user_id => user.id, :prompt_id => prompt.id)}
+	let(:vote){ response.votes.create(:votable_id => response.id, :votable_type => "Response", :polarity => 1 )}
 
+	it { should belong_to :prompt }
+	it { should belong_to :user }
+	it { should have_many :votes }
+	it { should validate_presence_of :body}
+	it { should validate_presence_of :user_id}
+	it { should validate_presence_of :prompt_id}
+	
+	it "should have an initial score of 0" do
+		expect(response.score).to eq 0
+	end
+
+	it "should update score when voted on" do
+		response.upvote(vote)
+		expect(response.score).to eq 1
+	end
 end
