6ef18e728d1da1e94ef0520436049d6475ef5cae
eddiekimosabe <eddiekimosabe@gmail.com>
2014-08-31 22:02:46 -0400

    Adding changes to votes_controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe VotesController do
 
-  let(:prompt) { FactoryGirl.create(:prompt) }
   let(:user) { FactoryGirl.create(:user) }
+  let(:prompt) { user.prompts.create(title: "What's your name?",
+    description: "Is it yeff?"
+  ) }
+  let(:response) { prompt.responses.create(
+  body: "Sleepy? He's like the mexican wolverine"
+  )}
 
   context 'votes on prompt' do
     it 'creates new vote on specified prompt with supplied session id' do
@@ -21,6 +26,22 @@ describe VotesController do
 
   end
 
+  context 'votes on response' do
+    it 'creates a new votes on specified response with the correct id' do
+      user 
+      prompt
+      response
+      session[:user_id] = user.id
+      expect{
+        post :createResponseVote, 
+          :vote => { 
+          :polarity => 1, 
+          :votable_id => response.id
+        }
+      }.to change{ Vote.all.count }.by(1)
+
+    end
+  end
 end
 
 
