2ae59409f7c4ffbfc2e9df9d3e9609aeb0a52b42
eddiekimosabe <eddiekimosabe@gmail.com>
2014-08-31 23:38:29 -0400

    Added some tests for prompt model.
    Cleaned up some code for readability.
    Added a users show erb for potential user profile page

--- spec/models/prompt_spec.rb
+++ spec/models/prompt_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 describe Prompt do
+	let(:user){ FactoryGirl.create(:user) }
+	let(:prompt){ user.prompts.create(title: "Spice Girls", description: "Who's the best?") }
 
+	it { should belong_to :user }
+	it { should have_many :responses }
+	it { should have_many :votes }
+	it { should validate_presence_of :title }
+	it { should validate_presence_of :description }
+	it { should validate_presence_of :user_id}
+	
+	# it "should have an initial score of 0" do
+	# 	prompt.score ||= 0
+	# end
+	
 end
